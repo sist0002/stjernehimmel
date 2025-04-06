14a4febeafdd7597871afd917966b88fe37465ad
signe <sist0002@stud.kea.dk>
2025-04-06 21:08:51 +0200

    forms js

---
+++ js/form.js
@@ -0,0 +1,26 @@
+// document.querySelector("#webform").reset();
+
+// const submit = document.querySelector("#submit_btn");
+
+// submit.addEventListener("click", submitForm);
+// document.querySelector("#reset_btn").addEventListener("click", reset);
+
+// function submitForm() {
+//     document.querySelector("#sum").style.display = "block";
+
+//     document.querySelector("#sumFname").textContent = document.querySelector("#f-name").value;
+//     document.querySelector("#sumLname").textContent = document.querySelector("#l-name").value;
+// }
+
+// document.querySelector("#sum").style.display = "block";
+
+document.querySelector("#sumFname").textContent = document.querySelector("#f-name").value;
+document.querySelector("#sumLname").textContent = document.querySelector("#l-name").value;
+
+// RADIOBUTTONS
+// document.querySelector("#sumContact").textContent = document.querySelector('input[name="radioContact"]:checked').value;
+
+// function reset() {
+//     document.querySelector("#webform").reset();
+//     document.querySelector("#sum").style.display = "none";
+//   }
